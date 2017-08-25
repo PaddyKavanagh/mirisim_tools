dc0a45ca0e9a8dab3bf61cec2c81934963c49ee1
Patrick Kavanagh <paddy.astro.dias@gmail.com>
2017-08-25 11:36:02 +0100

    corrected for spec2 dm fix

--- pipeline_mirisim.py
+++ pipeline_mirisim.py
@@ -39,8 +39,13 @@ def pipeline_mirisim(input_dir):
     """
 
     """
-        # set up logging
+    # set up logging
     log_file = os.path.join(os.path.abspath(input_dir),'pipeline_MIRISim.log')
+
+    # check if the pipeline log file exists
+    if os.path.isfile(log_file): os.remove(log_file)
+    else: pass
+
     testing_logger = logging.getLogger(__name__)
     testing_logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(log_file)
@@ -55,7 +60,6 @@ def pipeline_mirisim(input_dir):
     else:
         print("Simulation folder not found")
         sys.exit(0)
-
 
     # go through individual simulations and run through pipeline
     simulations = glob.glob(os.path.join(input_dir,'IMA*','20*'))
@@ -82,13 +86,6 @@ def pipeline_mirisim(input_dir):
 
         with datamodels.open(level1b_file) as level1b_dm:
 
-            # set up output plots
-            fig,axs = plt.subplots(1, 3)
-            fig.set_figwidth(15.0)
-            fig.set_figheight(5.0)
-            axs = axs.ravel()
-            # TODO improve colour scales for plots
-
             if level1b_dm.meta.exposure.type == 'MIR_IMAGE':
                 try:
                     level2a_dm = SloperPipeline.call(level1b_dm, output_file='rate.fits')
@@ -96,7 +93,14 @@ def pipeline_mirisim(input_dir):
 
                     # level2b_dm doesn't contain anything. report on GitHub
                     # for now read from fits file
-                    level2b_dm = datamodels.open('rat_cal.fits')
+                    #level2b_dm = datamodels.open('cal.fits')
+
+                    # set up output plots
+                    fig,axs = plt.subplots(1, 3)
+                    fig.set_figwidth(15.0)
+                    fig.set_figheight(5.0)
+                    axs = axs.ravel()
+                    # TODO improve colour scales for plots
 
                     # plot level 1b, 2a, 2b
                     axs[0].imshow(level1b_dm.data[0][-1], cmap='jet', interpolation='nearest', norm=LogNorm(), origin='lower')
@@ -105,6 +109,11 @@ def pipeline_mirisim(input_dir):
                     axs[1].annotate('level 2A', xy=(0.7,0.95), xycoords='axes fraction', fontsize=10, fontweight='bold', color='w')
                     axs[2].imshow(level2b_dm.data, cmap='jet', interpolation='nearest', norm=LogNorm(), origin='lower')
                     axs[2].annotate('level 2B', xy=(0.7,0.95), xycoords='axes fraction', fontsize=10, fontweight='bold', color='w')
+
+                    # save the pipeline plot
+                    out_fig_name = sim_name + '.pdf'
+                    fig.savefig(os.path.join(out_fig_dir,out_fig_name), dpi=200)
+                    del fig
 
                     # log pass
                     testing_logger.info('%s passed' % sim_name)
@@ -124,9 +133,12 @@ def pipeline_mirisim(input_dir):
                     level2a_dm = SloperPipeline.call(level1b_dm, output_file='rate.fits')
                     level2b_dm = Spec2Pipeline.call(level2a_dm, output_file='cal.fits')
 
-                    # level2b_dm doesn't contain anything. report on GitHub
-                    # for now read from fits file
-                    level2b_dm = datamodels.open('rat_cal.fits')
+                    # set up output plots
+                    fig,axs = plt.subplots(1, 3)
+                    fig.set_figwidth(15.0)
+                    fig.set_figheight(5.0)
+                    axs = axs.ravel()
+                    # TODO improve colour scales for plots
 
                     # plot level 1b, 2a, 2b
                     axs[0].imshow(level1b_dm.data[0][-1], cmap='jet', interpolation='nearest', norm=LogNorm(), origin='lower')
@@ -136,17 +148,17 @@ def pipeline_mirisim(input_dir):
                     axs[2].imshow(level2b_dm.data, cmap='jet', interpolation='nearest', norm=LogNorm(), origin='lower')
                     axs[2].annotate('level 2B', xy=(0.7,0.95), xycoords='axes fraction', fontsize=10, fontweight='bold', color='w')
 
+                    # save the pipeline plot
+                    out_fig_name = sim_name + '.pdf'
+                    fig.savefig(os.path.join(out_fig_dir,out_fig_name), dpi=200)
+                    del fig
+
                     # log pass
                     testing_logger.info('%s passed' % sim_name)
 
                 except Exception as e:
                     testing_logger.warning('%s failed' % sim_name)
                     testing_logger.warning('  %s: %s' % (e.__class__.__name__, str(e)))
-
-            # save the pipeline plot
-            out_fig_name = sim_name + '.pdf'
-            fig.savefig(os.path.join(out_fig_dir,out_fig_name), dpi=200)
-            del fig
 
         os.chdir(cwd)
 
